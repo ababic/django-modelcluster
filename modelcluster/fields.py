5e84039209dc528c813704831b3183516e8df7fc
Andy Babic <andyjbabic@gmail.com>
2019-10-14 13:18:12 +0100

    add some more stuff from ManyRelatedManager

--- modelcluster/fields.py
+++ modelcluster/fields.py
@@ -283,7 +283,30 @@ def create_deferring_forward_many_to_many_manager(rel, original_manager_cls):
             self.query_field_name = rel.field.related_query_name()
             self.prefetch_cache_name = rel.field.name
             self.source_field_name = rel.field.m2m_field_name()
+            self.target_field_name = rel.field.m2m_reverse_field_name()
             self.through = rel.through
+
+            self.source_field = self.through._meta.get_field(self.source_field_name)
+            self.target_field = self.through._meta.get_field(self.target_field_name)
+            self.core_filters = {}
+            self.pk_field_names = {}
+            for lh_field, rh_field in self.source_field.related_fields:
+                core_filter_key = '%s__%s' % (self.query_field_name, rh_field.name)
+                self.core_filters[core_filter_key] = getattr(instance, rh_field.attname)
+                self.pk_field_names[lh_field.name] = rh_field.name
+
+            self.related_val = self.source_field.get_foreign_related_value(instance)
+            if None in self.related_val:
+                raise ValueError('"%r" needs to have a value for field "%s" before '
+                                 'this many-to-many relationship can be used.' %
+                                 (instance, self.pk_field_names[self.source_field_name]))
+            # Even if this relation is not to pk, we require still pk value.
+            # The wish is that the instance has been already saved to DB,
+            # although having a pk value isn't a guarantee of that.
+            if instance.pk is None:
+                raise ValueError("%r instance needs to have a primary key value before "
+                                 "a many-to-many relationship can be used." %
+                                 instance.__class__.__name__)
 
         def get_original_manager(self):
             return original_manager_cls(self.instance)
@@ -362,9 +385,13 @@ def create_deferring_forward_many_to_many_manager(rel, original_manager_cls):
             )
 
         def _apply_rel_filters(self, queryset):
-            # Implemented as empty for compatibility sake
-            # But there is probably a better implementation of this function
-            return queryset._next_is_sticky()
+            """
+            Filter the queryset for the instance this manager is bound to.
+            """
+            queryset._add_hints(instance=self.instance)
+            if self._db:
+                queryset = queryset.using(self._db)
+            return queryset._next_is_sticky().filter(**self.core_filters)
 
         def _remove_prefetched_objects(self):
             try:
